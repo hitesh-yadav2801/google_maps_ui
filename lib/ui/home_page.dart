e5331b75b88a9fe9764f3e92d66483d7573f7c43
Hitesh Yadav <yhitesh7891@gmail.com>
2024-02-14 22:25:47 +0530

    first commit

---
+++ lib/ui/home_page.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:google_maps_ui/utils/constants/my_colors.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Container(
+          decoration: BoxDecoration(
+            color: Colors.grey.shade700,
+            borderRadius: BorderRadius.circular(40),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              IconButton(
+                onPressed: (){}, 
+                icon: SizedBox(
+                  height: 25,
+                  width: 25,
+                  child: Image.asset('lib/utils/assets/icons/google_maps.png'),
+                ),
+              ),
+              Expanded(
+                child: TextFormField(
+                  style: TextStyle(color: Colors.white),
+                  decoration: const InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'Search here',
+                    hintStyle: TextStyle(color: Colors.grey, fontWeight: FontWeight.w500),
+                  ),
+                ),
+              ),
+              IconButton(
+                onPressed: (){},
+                icon: const Icon(Icons.mic_rounded, color: Colors.white,)
+              ),
+              IconButton(
+                  onPressed: (){},
+                  icon: const Icon(Icons.account_circle_rounded, color: Colors.white,)
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/utils/constants/my_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class MyColors {
+  static const Color darkGreyColor = Color(0xFF30343C);
+  static const Color blackColor = Color(0xFF1F2125);
+  static const Color primaryColor = Color(0xFF4285F4);
+}
